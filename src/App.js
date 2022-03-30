788d9d62b2645988c42e02d3b88d0ed7f313c1e3
Tam Nguyen <tamquocnguyen@gmail.com>
2022-03-30 14:51:40 -0700

    working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return <div className="App">update</div>;
 }
 
 export default App;
